e1e4207277306e8b0cfead72d8cf6366df18b84c
teyyubmammadovh <teyyubmammadovh@gmail.com>
2023-08-10 23:42:48 +0400

    changes-end

--- src/components/Worker.jsx
+++ src/components/Worker.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Worker = ({ workerImg, workerName, workerRank, workerMedia }) => {
     return (
 
-            <div className="card col-12 col-md-6 col-lg-3 my-5 text-center bg-white" >
+            <div className="card worker-card col-12 col-md-6 col-lg-3 my-5 text-center bg-white" >
                 <img src={workerImg} className="card-img-top" alt="..." />
                 <div className="card-body">
                     <h5 className="card-title">{workerName} </h5>
